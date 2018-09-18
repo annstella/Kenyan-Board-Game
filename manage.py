d68c47e330dbd38857a9a3d08bb95d702ef50334
wangui.njoka <wangui.njoka@gmail.com>
2018-09-18 11:42:17 +0300

    folder strucuture

---
+++ manage.py
@@ -0,0 +1,25 @@
+from app import create_app
+# from app.models import
+# from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager,Server
+
+
+# Creating app instance
+# app = create_app('production')
+app = create_app('development')
+
+manager = Manager(app)
+#
+# migrate = Migrate(app,db)
+# manager.add_command('db',MigrateCommand)
+
+
+manager.add_command('server',Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app)
+
+if __name__ == '__main__':
+    manager.run()
