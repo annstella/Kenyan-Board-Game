a315c40137321aa9d1edf8a7ad48907c93cd0951
wangui.njoka <wangui.njoka@gmail.com>
2018-09-19 17:57:34 +0300

    created index page

--- js/player.js
+++ js/player.js
@@ -3,52 +3,38 @@ class Player {
         this.spot = 0;
     };
 
-    question() {
-        var userPrompt=prompt("Name one President of Kenya: ");
-        var pres=["Mwai Kibaki","Jomo Kenyatta","Uhuru Kenyatta","Daniel Moi"];
-        var presFound = false;
-        for(var x=0; x<pres.length; x++){
-            //console.log(pres[x]);
-                if(pres[x]===userPrompt){
-                    presFound = true;
-                    player.roll();
-                }
-            }
-                if(!presFound){
-                    alert('invalid answer');
-    }
 
 
-     if (player.roll()) {
-        var userPrompt1=prompt("Name any official language in Kenya: ");
-        var lang=["English","Kiswahili"];
-        var langFound = false;
-        for(var x=0; x<lang.length; x++){
-            //console.log(pres[x]);
-                if(lang[x]===userPrompt1){
-                    langFound = true;
-                    player.roll();
-                }
-            }
-                if(!langFound){
-                    alert('invalid answer');
-    }
-     } 
-
-    }
     
-   
-    roll() {
-        let die1 = document.getElementById('die1');
-        let status = document.getElementById('status'); 
-        let r = floor(random(1,7));
-        // let r =Math.floor(Math.random() * 6)
-        this.spot += r;
-        return die1.innerHTML = r;
+    var dice = {
+        sides: 6,
+        roll: function () {
+        var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        return randomNumber;
+  }
+}
+}
+function printNumber(number) {
+  var placeholder = document.getElementById('placeholder');
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById('button');
+
+button.onclick = function() {
+  var result = dice.roll();
+  printNumber(result);
+};
+        // let placeholder = document.getElementById('placeholder');
+        // // let status = document.getElementById('status');
+        // let r = floor(random(1,7));
+        // // let r =Math.floor(Math.random() * 6)
+        // this.spot += r;
+        // return placeholder.innerHTML = r;
         // status.innerHTML = "You rolled "+r+"";
 
     }
-    
+
     show(tile) {
         let current = tiles[this.spot];
         fill(255);
@@ -57,7 +43,6 @@ class Player {
 
 
 }
-    
-    
+
+
 }
- 
