72ea09cad25f0f9ec8f2020e73c0f0c705b925be
Michel Atieno <mishqamish@gmail.com>
2018-09-19 09:48:26 +0300

    Able to prompt for user to answer questionsi

--- js/player.js
+++ js/player.js
@@ -2,6 +2,40 @@ class Player {
     constructor() {
         this.spot = 0;
     };
+
+    question() {
+        var userPrompt=prompt("Name one President of Kenya: ");
+        var pres=["Mwai Kibaki","Jomo Kenyatta","Uhuru Kenyatta","Daniel Moi"];
+        var presFound = false;
+        for(var x=0; x<pres.length; x++){
+            //console.log(pres[x]);
+                if(pres[x]===userPrompt){
+                    presFound = true;
+                    player.roll();
+                }
+            }
+                if(!presFound){
+                    alert('invalid answer');
+    }
+
+
+     if (player.roll()) {
+        var userPrompt1=prompt("Name any official language in Kenya: ");
+        var lang=["English","Kiswahili"];
+        var langFound = false;
+        for(var x=0; x<lang.length; x++){
+            //console.log(pres[x]);
+                if(lang[x]===userPrompt1){
+                    langFound = true;
+                    player.roll();
+                }
+            }
+                if(!langFound){
+                    alert('invalid answer');
+    }
+     } 
+
+    }
     
    
     roll() {
