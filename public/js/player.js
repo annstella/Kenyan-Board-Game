ff806f3b7f24cec040141c8b33d3d27fee7aa902
Michel Atieno <mishqamish@gmail.com>
2018-09-20 22:17:35 +0300

    Fixed error

--- public/js/player.js
+++ public/js/player.js
@@ -1,46 +1,13 @@
+var game_on = true;
+var q_count = 0;
 class Player {
     constructor() {
         this.spot = 0;
     };
 
-    question() {
-        var userPrompt=prompt("Name one President of Kenya: ");
-        var pres=["Mwai Kibaki","Jomo Kenyatta","Uhuru Kenyatta","Daniel Moi"];
-        var presFound = false;
-        for(var x=0; x<pres.length; x++){
-            //console.log(pres[x]);
-                if(pres[x]===userPrompt){
-                    presFound = true;
-                    player.roll();
-                }
-            }
-                if(!presFound){
-                    alert('invalid answer');
-    }
-
-
-     if (player.roll()) {
-        var userPrompt1=prompt("Name any official language in Kenya: ");
-        var lang=["English","Kiswahili"];
-        var langFound = false;
-        for(var x=0; x<lang.length; x++){
-            //console.log(pres[x]);
-                if(lang[x]===userPrompt1){
-                    langFound = true;
-                    player.roll();
-                }
-            }
-                if(!langFound){
-                    alert('invalid answer');
-    }
-     }
-
-    }
-
-
     roll() {
         let die1 = document.getElementById('die1');
-        let status = document.getElementById('status');
+        let status = document.getElementById('status'); 
         let r = floor(random(1,7));
         // let r =Math.floor(Math.random() * 6)
         this.spot += r;
@@ -100,7 +67,7 @@ class Player {
 
                 // console.log(i);
                 
-                var response = window.prompt(questions[floor(random(1,11))].prompt)
+                var response = window.prompt(questions[floor(random(1,12))].prompt)
         
                     
             if(response != questions[i].answer){
@@ -136,12 +103,13 @@ class Player {
     
     show(tile) {
         let current = tiles[this.spot];
-        fill(0);
+        fill(70);
         let center = current.getCenter();
         return ellipse(center[0], center[1],32);
 
 
 }
-
-
+    
+    
 }
+ 
