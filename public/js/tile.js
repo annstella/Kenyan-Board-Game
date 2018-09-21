865fe6e40f8cb883de1d34537ffc307b3d7511fe
Michel Atieno <mishqamish@gmail.com>
2018-09-21 09:19:14 +0300

    Accept pull changes

--- public/js/tile.js
+++ public/js/tile.js
@@ -6,20 +6,11 @@ class Tile {
         this.index = index;
         this.next = next;
         if (this.index % 2 == 0){
-<<<<<<< HEAD:public/js/tile.js
             this.color =150; //change color of grid
         } else if (this.index % 5 == 0) {
             this.color = 120;
         } else if (this.index % 3 == 0) {
             this.color = 60;
-=======
-            this.color = 200; //change color of grid
-        } else if (this.index % 5 == 0) {
-            this.color = 100;
-        }
-         else {
-            this.color =100;
->>>>>>> development:js/tile.js
         }
          else {
             this.color =40; 
@@ -35,11 +26,7 @@ class Tile {
     show() {
         fill(this.color);
         rect(this.x, this.y, this.wh, this.wh);
-<<<<<<< HEAD:public/js/tile.js
         fill(220);
-=======
-        fill(255);
->>>>>>> development:js/tile.js
         textSize(25);
         text( this.next,this.x,this.y + this.wh)
     }
